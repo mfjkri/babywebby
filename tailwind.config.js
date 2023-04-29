26b061654873cb7a151d3e3346162a061057c53a
mfjkri <muhammadfikri6844@gmail.com>
2023-04-29 12:04:27 +0800

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#232426",
+        primary2: "#3b3b3b",
+        primary3: "#222426",
+        secondary: "#EEEEEE",
+        secondary2: "#A6A6A6",
+        secondary3: "#9EB1AF",
+        accent: "#00ADB5",
+        accent2: "#aa56ff",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
