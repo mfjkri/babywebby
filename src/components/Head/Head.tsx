9c0cf0979e0b270bcfb0cf12dd3f7c4d7fa2226c
mfjkri <muhammadfikri6844@gmail.com>
2023-04-29 14:45:26 +0800

    added head component

---
+++ src/components/Head/Head.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet-async";
+
+type HeadProps = {
+  title?: string;
+  description?: string;
+};
+
+/*
+Updates HTML document head (<head></head>) title and description for sub pages.
+
+Attributes:
+  - title: string | undefined
+    Sets the title tag in the head of the page. Defaults to empty string "".
+  
+  - description: string | undefined
+    Sets the description tag in the head of the page. Defaults to empty string "".
+*/
+export const Head = ({ title = "", description = "" }: HeadProps = {}) => {
+  return (
+    <Helmet title={title ? `${title} | Webby` : undefined} defaultTitle="Webby">
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+};
